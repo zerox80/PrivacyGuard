2f7c8d4658cce644d4558aaa4743b59b9f76cc43
zerox80 <rujbin@proton.me>
2025-11-09 23:18:09 +0100

    chore: upgrade Kotlin version from 1.9.23 to 1.9.24

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 
 plugins {
     id("com.android.application") version "8.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.9.23" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.24" apply false
 }
